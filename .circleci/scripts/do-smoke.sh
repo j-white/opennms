c1636398d4d78b3631251418290f8d65bed83a57
Jesse White <jesse@opennms.org>
2018-02-01 00:38:53 +0100

    Cleanup and use the actual built RPMs when running the smoke tests.

--- .circleci/scripts/do-smoke.sh
+++ .circleci/scripts/do-smoke.sh
@@ -1,36 +1,19 @@
 #!/bin/sh -e
 
-export OPENNMS_RPM_ROOT="/home/circleci/rpms"
-mkdir -p $OPENNMS_RPM_ROOT
-cd $OPENNMS_RPM_ROOT
-
-export URL_PREFIX="http://yum.opennms.org/bleeding/common/opennms"
-export RPM_VERSION="22.0.0-0.20180130.onms.develop.1291"
-wget -4 $URL_PREFIX/opennms-$RPM_VERSION.noarch.rpm
-wget -4 $URL_PREFIX/opennms-core-$RPM_VERSION.noarch.rpm
-wget -4 $URL_PREFIX/opennms-minion-$RPM_VERSION.noarch.rpm
-wget -4 $URL_PREFIX/opennms-minion-container-$RPM_VERSION.noarch.rpm
-wget -4 $URL_PREFIX/opennms-minion-features-core-$RPM_VERSION.noarch.rpm
-wget -4 $URL_PREFIX/opennms-minion-features-default-$RPM_VERSION.noarch.rpm
-wget -4 $URL_PREFIX/opennms-webapp-jetty-$RPM_VERSION.noarch.rpm
-
-git clone https://github.com/OpenNMS/opennms-system-test-api.git ~/stest-api || exit 1
+echo "#### Building Docker images..."
+git clone https://github.com/OpenNMS/opennms-system-test-api.git ~/stest-api
 cd ~/stest-api/docker
-# Debug
-echo "#### Files in RPM root"
+export OPENNMS_RPM_ROOT="${ARTIFACT_DIRECTORY}/rpms"
+echo "#### RPMs in ${OPENNMS_RPM_ROOT}:"
 ls $OPENNMS_RPM_ROOT
-echo "#### Determined release"
-export RELEASE=$(basename $OPENNMS_RPM_ROOT/opennms-minion-features-core-*.noarch.rpm | awk -F'-' '{ print $5; }')
-echo $RELEASE
 ./copy-rpms.sh
-echo "#### OpenNMS RPMs"
+echo "#### OpenNMS RPMs:"
 ls opennms/rpms
-echo "#### Minion RPMs"
+echo "#### Minion RPMs:"
 ls minion/rpms
-
 ./build-docker-images.sh
 
 cd ~/repo
-mvn -DupdatePolicy=never -DskipTests=true -DskipITs=true -P'!checkstyle' -Psmoke --projects :smoke-test --also-make clean install || exit 1
+mvn -DupdatePolicy=never -DskipTests=true -DskipITs=true -P'!checkstyle' -Psmoke --projects :smoke-test --also-make clean install
 cd ~/repo/smoke-test
-mvn -N -Dorg.opennms.smoketest.docker=true -DskipITs=false -Dtest=AboutPageIT integration-test
+mvn -N -Dorg.opennms.smoketest.docker=true -DskipTests=false -DskipITs=false integration-test
--- .circleci/scripts/post-build.sh
+++ .circleci/scripts/post-build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 mkdir -p ${ARTIFACT_DIRECTORY}
 mkdir -p ${ARTIFACT_DIRECTORY}/rpms
+# Screenshots taken by Selenium tests
+mkdir -p ${ARTIFACT_DIRECTORY}/screenshots
 mkdir -p ${ARTIFACT_DIRECTORY}/test-results/
 mkdir -p ${ARTIFACT_DIRECTORY}/targz
 mkdir -p ${ARTIFACT_DIRECTORY}/zip
 find . -type f -name "*.rpm" -exec cp -v {} ${ARTIFACT_DIRECTORY}/rpms \;
+find . -type f -regex ".*/target/screenshots/*" -exec cp -v {} ${ARTIFACT_DIRECTORY}/screenshots \;
 find . -type f -regex ".*/target/surefire-reports/.*xml" -exec cp {} ${ARTIFACT_DIRECTORY}/test-results/ \;
 find . -type f -regex ".*/target/failsafe-reports/.*xml" -exec cp {} ${ARTIFACT_DIRECTORY}/test-results/ \;
 find . -type f -name "*.tar.gz" -exec cp -v {} ${ARTIFACT_DIRECTORY}/targz \;
